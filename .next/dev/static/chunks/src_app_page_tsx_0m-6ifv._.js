6fb2844c48878f6f683da52570c37d09eae797b7
OmarAbovli <omarabovli@gmail.com>
2026-05-03 15:47:03 +0300

    Fix: Add .next to gitignore and finalize SSR fixes

--- .next/dev/static/chunks/src_app_page_tsx_0m-6ifv._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0g47.pw._.js","static/chunks/src_077ihob._.js","static/chunks/node_modules_063r_pb._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_components_Scene3D_tsx_12w4.yi._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_three_build_three_module_12n2-ho.js","static/chunks/0r.7_react-reconciler_0ji56..._.js","static/chunks/node_modules_@react-three_fiber_dist_events-776716bd_esm_06oyqx1.js","static/chunks/node_modules_troika-three-text_dist_troika-three-text_esm_0ph2c2t.js","static/chunks/node_modules_0d09lz9._.js","static/chunks/src_components_0be9s1h._.js"],
-    source: "dynamic"
-});
